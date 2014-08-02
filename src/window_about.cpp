7e2c4ffd321d37a221aac90894dd56fe34072c76
Andre Miranda <andreldm1989@gmail.com>
2014-08-02 13:40:47 -0300

    About window text translatable

--- src/window_about.cpp
+++ src/window_about.cpp
@@ -19,7 +19,7 @@ static void cb_close(Fl_Widget*, void*);
 
 void window_about_show(Fl_Window* parent)
 {
-    std::stringstream title;
+    std::stringstream sstream;
     Fl_Help_View* text;
     Fl_Group* group;
 
@@ -41,10 +41,10 @@ void window_about_show(Fl_Window* parent)
 
     window = new Fl_Window(window_x, window_y, window_w, window_h, _("About"));
 
-    title << "KISS Player v" << VERSION;
+    sstream << "KISS Player v" << VERSION;
 
     Fl_Box* box_title = new Fl_Box(5, 5, 240, 35);
-    box_title->copy_label(title.str().c_str());
+    box_title->copy_label(sstream.str().c_str());
     box_title->labelfont(FL_BOLD);
     box_title->labelsize(22);
     box_title->labeltype(FL_SHADOW_LABEL);
@@ -56,36 +56,49 @@ void window_about_show(Fl_Window* parent)
     /** INFO **/
     group = new Fl_Group(5, 60, window_w-10, window_h-80, _("Info"));
 
+    sstream.str(std::string());
+    sstream.clear();
+    sstream << "<b>" << _("A Simple and Lightweight Music Player") << "</b><br>";
+    sstream << "<p></p>";
+    sstream << _("Contact:") << " <u>" << PACKAGE_BUGREPORT << "</u><br>";
+    sstream << _("Website:") << " <u>" << PACKAGE_URL << "</u>";
+
     text = new Fl_Help_View(10, 65, window_w-20, 140);
     text->textsize(14);
-    text->value("<b>A Simple and Lightweight Music Player</b><br><br>\
-        Contact: <u>andreldm1989@gmail.com</u><br>\
-        Website: <u>https://sourceforge.net/projects/kissplayer</u><br><br></p>");
+    text->value(sstream.str().c_str());
 
     group->end();
 
     /** CREDITS **/
     group = new Fl_Group(5, 60, window_w-10, 210, _("Credits"));
 
+    sstream.str(std::string());
+    sstream.clear();
+    sstream << "<b>" << _("Developers:") << "</b><br>";
+    sstream << "André Miranda - " << _("Project Leader") << "<br>";
+    sstream << "Maykon Silva - " << _("Developer") << "<br>";
+    sstream << "<p></p>";
+    sstream << "<b>" << _("Contributors:") << "</b><br>";
+    sstream << "Herman Polloni";
+
     text = new Fl_Help_View(10, 65, window_w-20, 140);
     text->textsize(14);
-    text->value("<b>Developers:</b><br>\
-        André Miranda - Project Leader<br>\
-        Maykon Silva - Developer<br>\
-        <p></p>\
-        <b>Contributors:</b><br>\
-        Herman Polloni");
+    text->value(sstream.str().c_str());
 
     group->end();
 
     /** LICENSE **/
     group = new Fl_Group(5, 60, window_w-10, 210, _("License"));
 
+    sstream.str(std::string());
+    sstream.clear();
+    sstream << _("This program is licensed under the terms of the GNU General Public License version 2.") << "<br>";
+    sstream << _("Available online under:") << "<br>";
+    sstream << "<u>http://gnu.org/licenses/gpl-2.0.html</u>";
+
     text = new Fl_Help_View(10, 65, window_w-20, 140);
     text->textsize(14);
-    text->value("This program is licensed under the terms of the GNU General Public License version 2.<br>\
-        Available online under:<br>\
-        <u>http://gnu.org/licenses/gpl-2.0.html</u>");
+    text->value(sstream.str().c_str());
 
     group->end();
 
